e96f1f52688781a9fbc00cbc9c411e5c2c122404
angel <angeldrummer2000@gmail.com>
2024-08-01 06:46:10 -0700

    v1

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,26 +1,40 @@
 function ConvertHandler() {
   
   this.getNum = function(input) {
-    let result;
-    
-    return result;
+    const regex = /\d+(\.\d+)?(\/\d+)?/; //This is going to find any digits, and optionally followed by a decimal an other numbers
+    let result = input.match(regex); //THis is how I can filter for the numbers
+    return result ? result[0]: null;
   };
   
   this.getUnit = function(input) {
-    let result;
-    
+    const regex = /[a-z+A-Z]+/
+    let result = input.match(regex);
     return result;
   };
   
   this.getReturnUnit = function(initUnit) {
-    let result;
-    
+    const unitMap = {
+      "gal": "L",
+      "L": "gal",
+      "mi": "km",
+      "km": "mi",
+      "lbs": "kg",
+      "kg": "lbs"
+    };
+    let result = unitMap[initUnit.toLowerCase()]|| "invalid unit";
     return result;
   };
 
   this.spellOutUnit = function(unit) {
-    let result;
-    
+    const spelledOutUnits = {
+      "gal": "gallons",
+      "l": "liters",
+      "mi": "miles",
+      "km": "kilometers",
+      "lbs": "pounds",
+      "kg": "kilograms"
+    }
+    let result = spelledOutUnits[unit.toLowerCase()]  || "invalid unit";
     return result;
   };
   
@@ -29,13 +43,33 @@ function ConvertHandler() {
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     let result;
-    
+    switch (initUnit.toLowerCase()){
+      case "gal":
+        result = initNum * galToL;
+        break;
+      case "L":
+        result = initNum / galToL;
+        break;
+      case "lbs":
+        result = initNum * lbsToKg;
+        break;
+      case "kg":
+        result = initNum / lbsToKg;
+        break;
+      case "mi": 
+        result = initNum * miToKm;
+        break;
+      case "km":
+        result = initNum / miToKm;
+        break;
+      default:
+        result = "invalid unit";
+    }
     return result;
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
+    let result = `${initNum} ${this.spelledOutUnits(initUnit)} converts to ${returnNum.toFixed(5)} ${this.spelledOutUnits(returnUnit)}`;
     return result;
   };
   
--- routes/api.js
+++ routes/api.js
@@ -6,5 +6,24 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
+  
+  app.route('/api/convert').get((req,res)=>{
+    const input = req.body.input;
+
+    const initNum = convertHandler.getNum(input);
+    const initUnit = convertHandler.getUnit(input);
+    const returnNum = convertHandler.convert(initNum, initNum);
+    const returnUnit = convertHandler.getReturnUnit(initUnit);
+
+    const responseString = convertHandler.getString(initNum,initUnit,returnNum,returnUnit);
+    res.json({
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit,
+      string: responseString
+    })
+   
+  })
 
 };
